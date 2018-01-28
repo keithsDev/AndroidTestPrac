c009feb45a27fe2413d630798895fac58f957e56
Keith Kenneally <keithkenneally08@gmail.com>
2018-01-29 00:20:45 +0100

    Initial commit

---
+++ app/src/main/java/com/sampleapp2/app2/MainActivity.java
@@ -0,0 +1,62 @@
+package com.sampleapp2.app2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.graphics.Color;
+import android.widget.EditText;
+import android.content.res.Resources;
+import android.util.TypedValue;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_main);
+
+
+        //Layout
+        RelativeLayout myLayout = new RelativeLayout(this);
+        myLayout.setBackgroundColor(Color.GREEN);
+
+        //Button
+        Button redButton = new Button(this);
+        redButton.setText("Click me");
+        redButton.setBackgroundColor(Color.RED);
+
+        //Username input
+        EditText username = new EditText(this);
+
+        redButton.setId(1);
+        username.setId(2);
+
+        RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        RelativeLayout.LayoutParams usernameDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        //Give rules to position widgets
+        usernameDetails.addRule(RelativeLayout.ABOVE, redButton.getId());
+        usernameDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        usernameDetails.setMargins(0,0,0,50);
+
+        buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        Resources r = getResources();
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 200, r.getDisplayMetrics());
+
+        username.setWidth(px);
+
+        myLayout.addView(redButton, buttonDetails);
+        myLayout.addView(username, usernameDetails);
+        setContentView(myLayout);
+
+    }
+}
