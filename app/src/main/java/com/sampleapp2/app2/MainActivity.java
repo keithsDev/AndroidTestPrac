38dd5441977bd98b8cefd82bd506fbbc3658d0c9
Keith Kenneally <keithkenneally08@gmail.com>
2018-01-29 00:28:44 +0100

    third commit added comment

--- app/src/main/java/com/sampleapp2/app2/MainActivity.java
+++ app/src/main/java/com/sampleapp2/app2/MainActivity.java
@@ -1,5 +1,7 @@
+//testing app
 package com.sampleapp2.app2;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.RelativeLayout;
@@ -11,6 +13,7 @@ import android.util.TypedValue;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
